022982180531c51221ec9ae93a2d1faae28b70de
Kobus Viljoen <kobusv@spsi.co.za>
2017-06-22 15:09:34 +0200

    removed commented code

--- src/app/app.routing.js
+++ src/app/app.routing.js
@@ -1,23 +1,3 @@
-// export default class Routing {
-//   constructor($routeProvider, $locationProvider) {
-//     this.$locationProvider = $locationProvider;
-//     this.$routeProvider = $routeProvider;
-//   }
-//
-//   $locationProvider
-//       .html5Mode({
-//         enabled: false,
-//         requireBase: false
-//       });
-//
-//   $routeProvider
-//     .when('/', {
-//       controller: 'HomeCtrl',
-//       controllerAs: 'ctrl',
-//       template: require('./views/home.html')
-//     })
-// }
-
 function Routing($routeProvider, $locationProvider) {
   $locationProvider
       .html5Mode({
